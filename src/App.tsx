3b0a67e084c341de3ad28d45c82173fe5004a4b8
Aman <nurlanov.aman2@gmail.com>
2023-09-08 17:18:30 +0600

    Implemented request to get all pokemons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {PokemonCardComponent} from './components/card/PokemonCardComponent'
 import { PokemonsCompnent } from './components/lists/PokemonsList/PokemonsComponent';
 
 function App() {
--- src/components/lists/PokemonsList/PokemonsComponent.tsx
+++ src/components/lists/PokemonsList/PokemonsComponent.tsx
@@ -1,50 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Pokemons.scss'
 import { PokemonCardComponent } from '../../card/PokemonCardComponent'
+import { PokemonType, PokemonsResAPI } from '../../../APIs/PokeTypes';
+import PokeAPI from '../../../APIs/PokeAPI';
 
 export function PokemonsCompnent() {
+    const [data, setData] = useState<PokemonType[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const result: PokemonsResAPI = await new PokeAPI().GetPokemons();
+            console.log('result', result)
+            setData(result.results);
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
+        };
+
+        fetchData();
+    }, []);
+
+    console.log('data', data);
     return (
         <div className='Pokemons'>
-            <PokemonCardComponent 
-                pokeName='dfs'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+            {data.map((poke, index) => 
+            
+                <PokemonCardComponent 
+                    pokeName={poke.name}
+                    imgUrl={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`}
                 />
-            <PokemonCardComponent 
-                pokeName='dfstrgtr'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfsrtgrtgrt'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfsewfrew'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfres'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfs'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfstrgtr'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfsrtgrtgrt'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/8.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfsewfrew'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png'
-                />
-            <PokemonCardComponent 
-                pokeName='dfres'
-                imgUrl='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/10.png'
-                />
+            )}
         </div>
     )
 }

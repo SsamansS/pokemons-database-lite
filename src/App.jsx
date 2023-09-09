4d1f1803c4addd4037f9179eebb90a65673a7912
Aman <nurlanov.aman2@gmail.com>
2023-09-10 02:21:00 +0600

    Did the modal component of pokemon details

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import { PokemonsCompnent } from './components/lists/PokemonsList/PokemonsComponent';
-import PokemonDetails from './components/layouts/PokemonDetails/PokemonDetails';
 import Modal from './components/layouts/modal/Modal';
-import { Counter } from './features/counter/Counter';
-import { useSelector, useDispatch } from 'react-redux';
-import { openModal, closeModal } from './features/counter/counterSlice';
+import { useSelector } from 'react-redux';
 
 function App() {
   const isOpenModal = useSelector((state) => state.counter.isOpenModal)
-  // const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div>
-      <Counter/>
-      {/* <span>{`${isOpenModal}`}</span> */}
       <PokemonsCompnent/>
       <Modal isOpen={isOpenModal} setOpen={() => console.log('dfs')}>
         <h5>dsfsd</h5>
       </Modal>
-      {/* <button onClick={() => setIsOpen(true)}>classic open</button>
-      <Modal isOpen={isOpen} setOpen={() => setIsOpen(false)}>
-        <h5>dsfsd</h5>
-      </Modal> */}
     </div>
   );
 }
